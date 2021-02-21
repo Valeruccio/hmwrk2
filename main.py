9cfb181b727428e590f537ddbe13ce3085b548a8
Valeriy Tiulin <kugelprint@gmail.com>
2021-02-21 17:53:30 +0300

    Задание 1

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+print("Задание 1")
+
+user_list = ['Рамочно-сместительный двигатель', 5, 20.5, None, True]
+user_list.append("Маневровый двигатель")
+
+print("---")
+
+for x in user_list:
+    print(x)
+
+print("---")
+
+def user_type(x):
+ for x in range(len(user_list)):
+    print(type(user_list[x]))
+ return
+user_type(user_list)
+
+print("---")
+
 print("Задание 2")
